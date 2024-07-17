f4fd328e7216087896e72c028a74548d6bcf6c3f
Maxime Ancellin <maxime.ancellin@gmail.com>
2024-07-17 17:08:10 +0200

    Feature(argocd): add init project and app

--- cluster-setup.sh
+++ cluster-setup.sh
@@ -13,3 +13,13 @@ minikube start --kubernetes-version=${K8S_VERSION}
 helm repo add argo ${ARGO_REPO}
 helm repo update
 helm install argo-cd argo/argo-cd --version ${ARGO_VERSION} --namespace ${ARGO_NS} --create-namespace
+
+# Check Argo installation
+while [[ $(kubectl get pods -n "${ARGO_NS}" | grep -Ev "Running|Completed" | wc -l) -ne 1 ]];
+do
+  echo "Waiting for Argo installation..."
+  sleep 3
+done
+
+# Setup ArgoCD GitOps
+kubectl apply -f argocd/gitops.yaml -n ${ARGO_NS}
